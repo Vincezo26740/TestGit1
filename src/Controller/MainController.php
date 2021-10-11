2652707220160255f129e4184d317910cba73b9f
Vincezo26740 <87314393+Vincezo26740@users.noreply.github.com>
2021-10-11 16:19:14 +0200

    modif branche TestCmdr

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -11,6 +11,6 @@ class MainController extends AbstractController
      * @Route("/", name="main_home")
      */
     public function home(){
-        echo "hello the world TestCMDR";
+        echo "hello the world modification branche TestCMDR";
     }
 }
